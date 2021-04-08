c0ed5605eeb57819e35f21ad0934337ae4390871
Kiera Walton <kwalton@qa.com>
2021-04-08 13:53:26 +0100

    added age to hero details

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,16 +8,16 @@ import { Hero } from './hero';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Dr Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 11, name: 'Dr Nice', age: 23 },
+      { id: 12, name: 'Narco', age: 44 },
+      { id: 13, name: 'Bombasto', age: 30 },
+      { id: 14, name: 'Celeritas', age: 28 },
+      { id: 15, name: 'Magneta', age: 25 },
+      { id: 16, name: 'RubberMan', age: 29 },
+      { id: 17, name: 'Dynama', age: 20 },
+      { id: 18, name: 'Dr IQ', age: 56 },
+      { id: 19, name: 'Magma', age: 35 },
+      { id: 20, name: 'Tornado', age: 22 }
     ];
     return { heroes };
   }
